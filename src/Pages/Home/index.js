f7d7942967fa03e73acde319beb1105ed3bc7f44
AfnanKullab <af.kul.93@gmail.com>
2020-11-25 15:43:39 +0200

    delete-slider-from-main

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,14 +1,12 @@
 import React from "react";
 import Header from "../../Components/Header";
 import LiveClasses from "../../Components/LiveClasses";
-import Slider from "../../Components/Slider";
-
+ل
 export default function Home() {
   return (
     <div className="home">
       <Header />
       <LiveClasses />
-      <Slider />
     </div>
   );
 }
