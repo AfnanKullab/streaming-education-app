80684d2abe5fc7a461b8a452d6af36a92d2bf318
AfnanKullab <af.kul.93@gmail.com>
2020-11-25 15:51:23 +0200

    no-modify

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "../../Components/Header";
 import LiveClasses from "../../Components/LiveClasses";
-ل
+
 export default function Home() {
   return (
     <div className="home">
