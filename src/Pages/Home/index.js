f067bae96ba1ae5ac23c2e34447f8869d8462664
AfnanKullab <af.kul.93@gmail.com>
2020-11-26 16:19:38 +0200

    commit-theme

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../../Components/Header";
 import LiveClasses from "../../Components/LiveClasses";
+//import TopSlider from "../../Components/TopSlider";
 
 export default function Home() {
   return (
--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,7 @@
-const theme = {
+/*const theme = {
   pallette: {
     primary: "#130D4C",
     secondary: "#9e5f1b",
   },
 };
-export default theme;
+export default theme; */
