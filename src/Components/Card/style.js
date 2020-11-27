142b0f806657aef39719fd7e843fdfee555a1961
AfnanKullab <af.kul.93@gmail.com>
2020-11-27 18:03:18 +0200

    start-card-section

---
+++ src/Components/Card/style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+`;
+export const cardInfo = styled.div`
+  background-color: #f2ce9d;
+  color: #130d4c;
+  width: 195px;
+  height: 51.52vh;
+`;
