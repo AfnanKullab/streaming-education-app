b25ccabf1c42c54aa31a311dbc941105a4630d8d
AfnanKullab <af.kul.93@gmail.com>
2020-11-28 03:24:53 +0200

    Weekly Schedule

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,16 +1,17 @@
 import React from "react";
-// import Header from "../../Components/Header";
-// import LiveClasses from "../../Components/LiveClasses";
-import TopSlider from "../../Components/TopSlider";
-// import WeeklySchedule from "../../Components/WeeklySchedule";
+import Header from "../../Components/Header";
+import LiveClasses from "../../Components/LiveClasses";
+// import TopSlider from "../../Components/TopSlider";
+import Commonsection from "../../Components/Commonsection";
 
 export default function Home() {
   return (
     <div className="home">
-      {/* <Header />
-      <LiveClasses /> */}
-      <TopSlider />
-      {/* <WeeklySchedule title="Weekly Weekly" /> */}
+      <Header />
+      <LiveClasses />
+      {/* <TopSlider /> */}
+      <Commonsection title="Weekly Schedule" />
+      <Commonsection title="About Skooled" />
     </div>
   );
 }
